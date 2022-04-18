63d2de057ca79c25804b49bf12588e956112f142
Ivan Pazvantov <ipazvantov97@gmail.com>
2022-04-18 13:41:38 +0300

    Fisxed a line views_mixin.py

--- hoteliers/hoteliers/common/views_mixins.py
+++ hoteliers/hoteliers/common/views_mixins.py
@@ -4,6 +4,6 @@ from django.shortcuts import redirect
 class RedirectToHome:
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
-            return redirect('user home',)
+            return redirect('user home', request.user.pk)
 
         return super().dispatch(request, *args, **kwargs)
